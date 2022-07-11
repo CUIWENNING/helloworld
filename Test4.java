4648ff77a015028c5fca1ae295b10a4aba93806b
CUIWENNING <107614090+CUIWENNING@users.noreply.github.com>
2022-07-11 22:58:35 +0900

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,10 @@
+public class Test4 {
+   public static void main(String[] args) {
+      int x = 190;
+      while( x < 200 ) {
+         System.out.print("输出的值是：" + x + "....");
+         x++;
+         System.out.print("\n");
+      }
+   }
+}
