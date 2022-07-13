58c3c5ad48402f73a593219cc984993135eb7a43
CUIWENNING <107614090+CUIWENNING@users.noreply.github.com>
2022-07-13 23:53:38 +0900

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,13 @@
+public class Test8{
+	public static void main(String[] args) {
+		int [] numbers = {10,20,30,40,50};
+		
+		for(int x :numbers) {
+			if(x ==40) {
+				break;
+			}
+			System.out.println(x);
+			System.out.println("\n");
+		}
+	}
+}
